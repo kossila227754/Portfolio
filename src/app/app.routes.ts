5e44366b112c38a7fc283855edc5c3fc1ff67496
kossila227754 <komedja.david@hotmail.com>
2025-03-23 18:40:28 +0100

    v2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: ContactComponent }
